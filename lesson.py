f2b2dba9eab4d81d3854f109fe17ffdb94fe3bae
ColinPhilipJohnstone <colinjohnstone@gmail.com>
2021-07-20 22:32:32 +0200

    clean up of main

--- lesson.py
+++ lesson.py
@@ -1,4 +1,4 @@
 
-class lesson:
+class Lesson:
     def __init__():
         pass
--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 
 import pygame
 import menu
+import lesson
 from parameters import params
 import lingqAPI as api
 
@@ -20,26 +21,46 @@ class App:
         # should end execution if False
         self._running = True
 
-        # start screens as list 
-        self._screens = []
-        self._currentScreen = menu.Menu()
+        # start screens but only load the menu now (load a lesson if selected)
+        self._menuScreen = menu.Menu()
+        self._lessonScreen = None
+        self._currentScreen = self._menuScreen
+
+        # set tracker for drawing the contents
+        self._render = True
 
 
 
     def _onEvent(self):
         for event in pygame.event.get():
             
-            self._running = self._currentScreen.onEvent(event)
+            # send event to current screen (menu or lesson) and get four bits of 
+            # information. These are
+            #  1. if should end simulation
+            #  2. if should redraw
+            #  3. id of lesson to load ("" for not doing anything, "menu" for switching to menu)
+            shouldEnd, self._render, lessonId = self._currentScreen.onEvent(event)
 
-            if event.type == pygame.QUIT:
+            # check if event type is for quitting
+            if shouldEnd or (event.type==pygame.QUIT):
                 self._running = False
-                
+
+            # check for loading menu or lesson
+            if lessonId == "":
+                pass
+            elif lessonId == "menu":
+                self._currentScreen = self._menuScreen
+            else:
+                self._lessonScreen = lesson.Lesson(lessonId)
+                self._currentScreen = self._lessonScreen
 
     def _onLoop(self):
         pass
 
     def _onRender(self):
-        self._currentScreen.onRender(self._window)
+        if self._render:
+            self._currentScreen.onRender(self._window)
+            self._render = False
 
     def execute(self):
 
