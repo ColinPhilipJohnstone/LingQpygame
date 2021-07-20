ce8d4bc088290ca84d9aa4f1aded694f88d8bc30
ColinPhilipJohnstone <colinjohnstone@gmail.com>
2021-07-20 22:34:36 +0200

    add lesson class structure

--- lesson.py
+++ lesson.py
@@ -1,4 +1,21 @@
+from params import params
+import pygame
 
 class Lesson:
-    def __init__():
-        pass
+
+    def __init__(self):
+        '''Sets up a lesson'''
+        
+
+    def onEvent(self,event):
+        '''Handles what to do when something happens'''
+
+        return true
+    
+    def onRender(self,window):
+
+        # background
+        window.fill(params["MENU_BACKGROUND_COLOR"])
+
+        # update display
+        pygame.display.update()
